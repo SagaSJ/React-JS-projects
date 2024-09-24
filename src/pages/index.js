5c579ebd729140e0c76689d45330fcce27b77d93
SagayaRaj <sagayar831@gmail.com>
2024-09-24 17:36:29 +0530

    Initial upload of ReactJS project

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "../pages/HomePage/HomePage";
+import Error from "../pages/ErrorPage/ErrorPage";
+import MealDetails from "../pages/MealDetailsPage/MealDetailsPage";
+import Category from "../pages/Category/CategoryPage";
+
+export { Home, Error, MealDetails, Category };
